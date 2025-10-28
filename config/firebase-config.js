45276dc4a4230f37c863839bbb121ff730c636f0
Igor Santana <iss@santoantoniobicas.local>
2025-10-28 14:14:42 -0300

    Add: config firebase e remover do gitignore

---
+++ config/firebase-config.js
@@ -0,0 +1,11 @@
+// Configuração do Firebase para is-dashs
+const firebaseConfig = {
+    apiKey: "AIzaSyBd3B66JI5IRdvbRCx7zZazIvMiXa3De_c",
+    authDomain: "is-dashs.firebaseapp.com",
+    projectId: "is-dashs",
+    storageBucket: "is-dashs.firebasestorage.app",
+    messagingSenderId: "642223381069",
+    appId: "1:642223381069:web:d73dd578559e2a5bdf5a04",
+    measurementId: "G-01RMYT2NWS"
+};
+
